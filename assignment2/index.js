41bb99385bb53576f7cd4e072f926839edfca763
Hanxiao Mao <hanxmao@hotmail.com>
2022-11-14 16:34:15 -0800

    fix empty wrong tag bug

--- assignment2/index.js
+++ assignment2/index.js
@@ -2,7 +2,7 @@ var currentPage = 1
 var pageSize = 3
 
 const display = function(){
-    $('main').empty()
+    $('#contentArea').empty()
     $.ajax({
         url:"https://api.themoviedb.org/3/movie/top_rated?api_key=96871b776d98641185bbfa8afae68416&language=en-US",
         type: "GET",
@@ -14,7 +14,7 @@ const display = function(){
             startIndex = (currentPage - 1) * pageSize
             endIndex = startIndex + pageSize
             for(i = startIndex; i < endIndex; i++ ){
-                $('main').append(
+                $('#contentArea').append(
                     `
                     <div>
                         ${data.results[i].title}<br>
@@ -32,19 +32,22 @@ const display = function(){
 setup = function(){
     $('select').change(()=>{
         console.log( $("select option:selected").val());
-        currentPage = $("select option:selected").val()
- 
-    })
-    $('body').on('click', ".backdropBtn", function(){
-        path = $(this).attr("get_path")
-        $('aside').html(
-            `
-            <img src='https://image.tmdb.org/t/p/w500${path}'>
-            `
-        )
-    })
+        pageSize = Number($("select option:selected").val())
+        
+        display()
+    }) 
 }
 
 display()
+
+$('body').on('click', ".backdropBtn", function(){
+    path = $(this).attr("get_path")
+    $('aside').html(
+        `
+        <img src='https://image.tmdb.org/t/p/w500${path}'>
+        `
+    )
+})
+
 
 $(document).ready(setup)
