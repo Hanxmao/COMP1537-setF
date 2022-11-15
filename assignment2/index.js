07dc70ac31459df033ff341d836df21099d8d85b
Hanxiao Mao <hanxmao@hotmail.com>
2022-11-14 16:06:20 -0800

    Page select menu done, data grab through api

---
+++ assignment2/index.js
@@ -0,0 +1,40 @@
+var currentPage = 1
+var pageSize = 3
+
+setup = function(){
+    $.ajax({
+        url:"https://api.themoviedb.org/3/movie/top_rated?api_key=96871b776d98641185bbfa8afae68416&language=en-US",
+        type: "GET",
+        success: function(data){
+            console.log(data)
+            // for(i=0; i<data.results.length; i++){
+            //     console.log(data.results[i].title);
+            // }
+            startIndex = (currentPage - 1) * pageSize
+            endIndex = startIndex + pageSize
+            for(i = startIndex; i < endIndex; i++ ){
+                $('main').append(
+                    `
+                    <div>
+                        ${data.results[i].title}<br>
+                        <p>${data.results[i].overview}</p>
+                        <img src='https://image.tmdb.org/t/p/w500${data.results[i].poster_path}'>
+                        <button get_path="${data.results[i].poster_path}" class="backdropBtn">Get Backdrop Image</button>
+                    </div>
+                    `
+                )
+            }
+        }
+    })
+
+    $('body').on('click', ".backdropBtn", function(){
+        path = $(this).attr("get_path")
+        $('aside').html(
+            `
+            <img src='https://image.tmdb.org/t/p/w500${path}'>
+            `
+        )
+    })
+}
+
+$(document).ready(setup)
