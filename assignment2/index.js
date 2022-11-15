48fef2e5222d6cad50d31ce62a487b5d85d8a58c
Hanxiao Mao <hanxmao@hotmail.com>
2022-11-14 16:22:41 -0800

    Grap the data from api by select menu size value

--- assignment2/index.js
+++ assignment2/index.js
@@ -1,12 +1,13 @@
 var currentPage = 1
 var pageSize = 3
 
-setup = function(){
+const display = function(){
+    $('main').empty()
     $.ajax({
         url:"https://api.themoviedb.org/3/movie/top_rated?api_key=96871b776d98641185bbfa8afae68416&language=en-US",
         type: "GET",
         success: function(data){
-            console.log(data)
+            // console.log(data)
             // for(i=0; i<data.results.length; i++){
             //     console.log(data.results[i].title);
             // }
@@ -26,7 +27,14 @@ setup = function(){
             }
         }
     })
+}
 
+setup = function(){
+    $('select').change(()=>{
+        console.log( $("select option:selected").val());
+        currentPage = $("select option:selected").val()
+ 
+    })
     $('body').on('click', ".backdropBtn", function(){
         path = $(this).attr("get_path")
         $('aside').html(
@@ -36,5 +44,7 @@ setup = function(){
         )
     })
 }
+
+display()
 
 $(document).ready(setup)
