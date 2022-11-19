41042b7c06685f22de5e6fa4c28355d2ef71cfc2
Hanxiao Mao <hanxmao@hotmail.com>
2022-11-18 23:15:22 -0800

    Refactor the code to make the button and content change according to the selected value

--- assignment2/index.js
+++ assignment2/index.js
@@ -4,34 +4,27 @@ let pageNumber = null
 var results = null
 
 
+
 const display = function(){
+    console.log(results);
     $('#contentArea').empty()
-    // $('#buttonArea').empty()
-    let searchTerm = $('#searchTerm').val()
-    $.ajax({
-        url:`https://api.themoviedb.org/3/search/movie?api_key=96871b776d98641185bbfa8afae68416&language=en-US&query=${searchTerm}&page=1&include_adult=false`,
-        type: "GET",
-        success: function(data){
-            results = data.results
-            addButton()
-            startIndex = (currentPage - 1) * pageSize
-            endIndex = startIndex + pageSize
-            for(i = startIndex; i < endIndex; i++ ){
-                $('#contentArea').append(
-                    `
-                    <div>
-                    <p>#${i + 1}</p>
-                    <p>Title: ${results[i].title}</p>
-                    <p>Description: ${results[i].overview}</p>
-                    <img src='https://image.tmdb.org/t/p/w500${results[i].poster_path}'>
-                    <button get_path="${results[i].poster_path}" class="backdropBtn">Get Backdrop Image</button>
-                    </div>
-                    `
-                    )
-                }
-                
-            }
-        })
+    startIndex = (currentPage - 1) * pageSize
+    endIndex = startIndex + pageSize
+    for(i = startIndex; i < endIndex; i++ ){
+        $('#contentArea').append(
+            `
+            <div>
+            <p>#${i + 1}</p>
+            <p>Title: ${results[i].title}</p>
+            <p>Description: ${results[i].overview}</p>
+            <img src='https://image.tmdb.org/t/p/w500${results[i].poster_path}'>
+            <button get_path="${results[i].poster_path}" class="backdropBtn">Get Backdrop Image</button>
+            </div>
+            `
+            )
+        }
+
+   
     }
     
 const addButton = function(){
@@ -42,10 +35,8 @@ const addButton = function(){
         $("#buttonArea").append(`<button value="${i}" id="${i}">${i}</button>`)
         $('body').on('click', `#${i}`, function(){
             currentPage = Number($(this).val())
+            display()
             console.log(currentPage);   
-        })
-        $(`#${i}`).click(()=>{
-            currentPage = i
         })
     }
     $("#buttonArea").append('<button id="next" >next</button>')
@@ -54,34 +45,48 @@ const addButton = function(){
 
     $("#first").click(()=>{
         currentPage = 1
+        display()
         console.log(currentPage);
     })
     $("#prev").click(()=>{
         if(currentPage > 1){
             currentPage -= 1
+            display()
             console.log(currentPage);
         }
     })
     $("#next").click(()=>{
         if(currentPage < Math.ceil(results.length / pageSize)){
             currentPage += 1
+            display()
             console.log(currentPage);
         }
     })
     $("#last").click(()=>{
         currentPage = Math.ceil(results.length / pageSize)
+        display()
         console.log(currentPage);
     })
 }
 
 setup = function(){
-    $('select').change(()=>{
-        // console.log( $("select option:selected").val());
-        pageSize = Number($("select option:selected").val())
-        display()
-        
-    }) 
+    $.ajax({
+        url:`https://api.themoviedb.org/3/search/movie?api_key=96871b776d98641185bbfa8afae68416&language=en-US&query=${$('#searchTerm').val()}&page=1&include_adult=false`,
+        type: "GET",
+        success: function(data){
+            results = data.results
+        }
+    })
     $('#searchBtn').click(()=>{
+        let searchTerm = $('#searchTerm').val()
+        $.ajax({
+            url:`https://api.themoviedb.org/3/search/movie?api_key=96871b776d98641185bbfa8afae68416&language=en-US&query=${searchTerm}&page=1&include_adult=false`,
+            type: "GET",
+            success: function(data){
+                results = data.results
+            }
+        })
+        
         display()
         $('body').on('click', ".backdropBtn", function(){
             path = $(this).attr("get_path")
@@ -93,6 +98,14 @@ setup = function(){
             )
         })
     })
+
+    $('select').change(()=>{
+        // console.log( $("select option:selected").val());
+        pageSize = Number($("select option:selected").val())
+        addButton()
+        display()
+        
+    }) 
 }
 
 
